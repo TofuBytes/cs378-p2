79046a42ff1c60f9df07753adb2e7e96c11f1cc8
Fluffau <49350466+Fluffau@users.noreply.github.com>
2023-02-09 05:18:34 -0600

    amac add

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+var orderbt = document.getElementById("order-items");
+var macInc = document.getElementById("mac-inc");
 
+//increment event listeners
+macInc.addEventListener("click", incQty);
+orderbt.addEventListener("click", orderAlert);
 
+function incQty(){
+    //alert("changing quantity");
+    var num = document.getElementById("qty-mac").textContent;
+    //alert(num);
+    document.getElementById("qty-mac").innerHTML = parseInt(num,10) + 1;
+    //qty = qty + 1;
+}
+
+function orderAlert(){
+    //just testing to see if it works for now
+    alert("Order Placed!")
+}
 //alert('hello world');
